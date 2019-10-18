2beeac036b03642b526cd169bf31334be9fe324c
yibo.xiao <yibo.xiao@ly.com>
2019-10-18 11:32:40 +0800

    lesson001

---
+++ scalaDemo/src/com/example/xiao/Lesson001.scala
@@ -0,0 +1,21 @@
+package com.example.xiao
+
+/**
+  *
+  * created on 19/10/17.
+  * time: 17:11
+  *
+  * @author yibo.xiao
+  *
+  *
+  */
+
+
+object Lesson001 {
+
+  def main(args: Array[String]): Unit = {
+    var seqs = 1 until(10)
+    println(seqs)
+  }
+
+}
